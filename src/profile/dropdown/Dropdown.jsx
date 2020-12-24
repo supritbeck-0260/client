b19d6aea0edf72501da90dfcd685a4d5550b213d
Suprit <supritbeck@gmail.com>
2020-12-24 23:23:33 +0530

    dropdown position fixed

--- src/profile/dropdown/Dropdown.jsx
+++ src/profile/dropdown/Dropdown.jsx
@@ -7,7 +7,7 @@ import {NavLink} from 'react-router-dom';
 const useStyles = makeStyles((theme) => ({
     dropdownMenu:{
         background:'white',
-        zIndex:'1111',
+        zIndex:'100',
         width:'fit-content',
         position:'absolute',
         border:'2px solid silver',
@@ -19,10 +19,16 @@ const useStyles = makeStyles((theme) => ({
         borderRadius:'5px',
         display:'flex',
         justifyContent:'center',
-        flexDirection:'column'
+        flexDirection:'column',
+        maxHeight: '300px',
+        overflowY: 'auto',
     },
     li:{
         display:'flex',
+    },    
+    lis:{
+        display:'flex',
+        background:'#E8E8E8'
     },
     navLink:{
         textDecoration:'none',
@@ -36,13 +42,23 @@ const useStyles = makeStyles((theme) => ({
         height:'30px',
         margin:'0px 5px 5px 0px',
     },
+    heading:{
+        textTransform: 'uppercase',
+        textAlign:'center',
+        fontWeight:'bold'
+    },
+    loader:{
+        display:'flex',
+        justifyContent:'center',
+        width:'100px',
+        height:'45px',
+    }
 }));
 const Dropdown = (props)=>{
     const classes = useStyles();
     const [list,setList] = useState([]);
 useEffect(()=>{
     Axios.post(process.env.REACT_APP_SERVER_URL+'/mentor/list/',{id:props.id,type:props.type}).then(response=>{
-        console.log(response);
         switch(response.status){
             case 200:
                 setList(response.data);
@@ -52,14 +68,15 @@ useEffect(()=>{
 },[]);
     return(
         <>
-        <ul className={classes.dropdownMenu}>
+        <ul className={classes.dropdownMenu} style={{right:props.type=='mentors'?'0px':''}}>
+            <li className={classes.heading}>{props.type} <hr/></li>
            {list.length? list.map((value,index)=>
-           <li className={classes.li} key={index}>
+           <li className={index%2==0?classes.li:classes.lis} key={index}>
             <NavLink className={classes.navLink} to={value.uid}>
              <Avatar className={classes.avatar} src={process.env.REACT_APP_SERVER_URL+'/profile/'+value.avatar}/> {value.name}
              </NavLink>
             </li>
-           ):<CircularProgress/>}
+           ):<li className={classes.loader}><CircularProgress/></li>}
         </ul>
         </>
     )

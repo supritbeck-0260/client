004174420b0ae700a48c0422ebd176214be83d2b
Suprit <supritbeck@gmail.com>
2021-01-31 17:28:27 +0530

    intigrated graph with backend

--- src/Admin/Chart/Chart.jsx
+++ src/Admin/Chart/Chart.jsx
@@ -1,5 +1,6 @@
 import React , {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Axios from 'axios';
 
 import Chart from 'chart.js';
 const useStyles = makeStyles({
@@ -12,21 +13,39 @@ const useStyles = makeStyles({
 const Graph = () => {
     
     useEffect(()=>{
+        Axios.get(process.env.REACT_APP_SERVER_URL+'/analysis/show').then(response=>{
+            const graphInfo = response.data;
+            let labels = [];
+            let data = [];
+            for (let value in graphInfo){
+                labels.push(value);
+                data.push(graphInfo[value].length);
+            }
         var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','teal', 'maroon'],
+                labels: labels,
                 datasets: [{
                     label: 'Click Frequency',
-                    data: [12, 19, 3, 5, 2, 3,100, 15],
+                    data: data,
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor:'rgba(255, 159, 64, 1)',
                     borderWidth: 1
                 }]
             },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
         });
-    });
+     });
+    },[]);
     const classes = useStyles();
     return (
         <>

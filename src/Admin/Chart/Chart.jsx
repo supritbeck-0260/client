7d6f4f783dc78932a261f79c3191aef915c578be
Suprit <supritbeck@gmail.com>
2021-01-31 16:19:51 +0530

    removed unwanted lines

--- src/Admin/Chart/Chart.jsx
+++ src/Admin/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Chart from 'chart.js';
 const useStyles = makeStyles({
     container:{
-        width:'600px',
+        width:'800px',
         height:'500px',
         margin:'auto'
     }
@@ -16,32 +16,15 @@ const Graph = () => {
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','teal', 'maroon'],
                 datasets: [{
                     label: 'Click Frequency',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [12, 19, 3, 5, 2, 3,100, 15],
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                    // borderColor: [
-                    //     'rgba(255, 99, 132, 1)',
-                    //     'rgba(54, 162, 235, 1)',
-                    //     'rgba(255, 206, 86, 1)',
-                    //     'rgba(75, 192, 192, 1)',
-                    //     'rgba(153, 102, 255, 1)',
-                    //     'rgba(255, 159, 64, 1)'
-                    // ],
                     borderColor:'rgba(255, 159, 64, 1)',
                     borderWidth: 1
                 }]
             },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
         });
     });
     const classes = useStyles();
